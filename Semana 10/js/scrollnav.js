3dbf2bb8ca4ae8a5bdce9ce56c5e37433fb4e242
Eric Zapata <admin@ZombieJericohs-MacBook-Pro.local>
2015-11-14 00:55:01 -0400

    Semana_10

---
+++ Semana 10/js/scrollnav.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('nav').affix({
+		offset:{
+			top: $('header').height()
+		}
+	$('nav').on('affix.bs.affix', function(){
+		$('body').css('margin-top', $('nav').height());
+	});
+
+	$('nav').on('affix.top.bs.affix', function(){
+		$('body').css('margin-top', 0);
+	});
+});
